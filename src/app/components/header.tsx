d55132b495fc9e729335c41ead3b309eb6f108dd
Nguyễn Thế Trường <nttruong1006@gmail.com>
2024-10-05 11:48:42 +0700

    Header.tsx

---
+++ src/app/components/header.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { cookies } from "next/headers";
+
+const Header = () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token");
+
+  console.log(">>> token:", token?.value);
+
+  return (
+    <header className="border-b border-neutral-300">
+      <div className="container h-14 flex items-center justify-between gap-4">
+        <div className="space-x-4">
+          <Link href="/">Home</Link>
+          {token && <Link href="/profile">Profile</Link>}
+        </div>
+
+        {token ? (
+          <Link href={`/auth/sign-out?token=${token.value}`}>Sign out</Link>
+        ) : (
+          <div className="space-x-4">
+            <Link href="/auth/sign-in">Sign in</Link>
+            <Link href="/auth/sign-up">Sign up</Link>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
